c0c06422f3b9c074b8b54ec77fc3808f996b8e7e
jacob shoemaker <jacob.shoemaker.travel@gmail.com>
2023-02-23 16:20:07 -0700

    MechaCar Statistical Analysis Folder

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,4 @@
-//Deliveriable 1
+//DELIVERIABLE 1
 
 library(dplyr)
 
@@ -14,9 +14,9 @@ setwd("~/Desktop/MechaCar_Statistical_Analysis")
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 data(mtcars)
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
-summary(model)
+summary(mymodel)
 
-//Deliveriable 2
+//DELIVERIABLE 2
 
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
@@ -26,8 +26,9 @@ Lot_summary_df
 total_summary_df <- summarize(Lot_summary_df, mean_PSI = mean(mean_PSI), median_PSI = mean(median_PSI), var_PSI = sum(var_PSI), sd_PSI = sqrt(sum(var_PSI)))
 total_summary_df
 
-//Deliveriable 3 
+//DELIVERIABLE 3 
 
+1.
 t.test(total_summary, mu = 1500)
 
 data:  total_summary
@@ -39,4 +40,42 @@ sample estimates:
 mean of x 
 797.5961 
 
+2.
+> t.test(Lot_summary_df$mean_PSI, mu = 1500, subset = suspension$Manufacturing_Lot == "Lot1")
+
+One Sample t-test
+data:  Lot_summary_df$mean_PSI
+t = -0.92336, df = 2, p-value = 0.4533
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+  1493.095 1504.465
+sample estimates:
+  mean of x 
+1498.78 
+
+t.test(Lot_summary_df$median_PSI, mu = 1500, subset = var_PSI$Manufacturing_Lot == "Lot2")
+
+One Sample t-test
+data:  Lot_summary_df$median_PSI
+t = -1, df = 2, p-value = 0.4226
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+  1497.349 1501.651
+sample estimates:
+  mean of x 
+1499.5 
+
+t.test(Lot_summary_df$var_PSI, mu = 1500, subset = sd_PSI$Manufacturing_Lot == "Lot3")
+
+One Sample t-test
+data:  Lot_summary_df$var_PSI
+t = -26.007, df = 2, p-value = 0.001475
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+  -178.7265  297.8832
+sample estimates:
+  mean of x 
+59.57837 
+
+//DELIVERIABLE 4
 
