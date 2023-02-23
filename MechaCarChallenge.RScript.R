1579e683e0a12eb3f2c0060b16f100eccd581aa2
jacob shoemaker <jacob.shoemaker.travel@gmail.com>
2023-02-23 16:21:37 -0700

    MechaCar Statistical Analysis Folder

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -14,7 +14,7 @@ setwd("~/Desktop/MechaCar_Statistical_Analysis")
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 data(mtcars)
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
-summary(mymodel)
+summary(model)
 
 //DELIVERIABLE 2
 
